166f512ccdf20747ba9cf9cdd0567fe9d0010504
coderLin <1543099786@qq.com>
2025-09-03 21:12:55 +0800

    feat: 接入deepseek

--- src/test/java/com/example/XiaozhiAppTest.java
+++ src/test/java/com/example/XiaozhiAppTest.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import org.example.XiaozhiApp;
 import org.junit.jupiter.api.Test;
@@ -30,12 +31,13 @@ public class XiaozhiAppTest {
      * 整合SpringBoot
      */
     @Autowired
-    private OpenAiChatModel openAiChatModel;
+//    private OpenAiChatModel openAiChatModel;
+    private ChatLanguageModel chatLanguageModel;
 
     @Test
     public void testSpringBoot() {
         //向模型提问
-        String answer = openAiChatModel.chat("你好");
+        String answer = chatLanguageModel.chat("你好");
         //输出结果
         System.out.println(answer);
     }
