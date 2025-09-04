f9d8be2e1f0c6d688eaa3a9a2d189fae6e115d21
coderLin <1543099786@qq.com>
2025-09-04 21:23:17 +0800

    feat: 接入mongodb

--- src/test/java/com/example/ChatAppTest.java
+++ src/test/java/com/example/ChatAppTest.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = ChatApp.class)
-public class XiaozhiAppTest {
+public class ChatAppTest {
     /**
      * gpt-4o-mini语言模型接入测试
      */
