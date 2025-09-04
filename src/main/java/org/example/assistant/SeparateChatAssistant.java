a589024efa4b8b3d3ebe70bd6f5404f00ef7905e
coderLin <1543099786@qq.com>
2025-09-04 22:36:03 +0800

    feat: 系统提示词

--- src/main/java/org/example/assistant/SeparateChatAssistant.java
+++ src/main/java/org/example/assistant/SeparateChatAssistant.java
@@ -1,6 +1,7 @@
 package org.example.assistant;
 
 import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 
@@ -19,6 +20,7 @@ public interface SeparateChatAssistant {
      * @param userMessage 用户消息
      * @return
      */
+    @SystemMessage("你是我的好朋友，请用东北话回答问题。今天是{{current_date}}")
     String chat(@MemoryId int memoryId, @UserMessage String userMessage);
 }
 
---
+++ src/test/java/com/example/PromptTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.example.ChatApp;
+import org.example.assistant.SeparateChatAssistant;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ChatApp.class)
+public class PromptTest {
+
+    // 系统提示词
+    @Autowired
+    private SeparateChatAssistant separateChatAssistant;
+    @Test
+    public void testSystemMessage() {
+        String answer1 = separateChatAssistant.chat(3,"你好，我是李大白");
+        System.out.println(answer1);
+        String answer2 = separateChatAssistant.chat(3,"今天几号");
+        System.out.println(answer2);
+    }
+}
