2191390c66691b8ea5868a1a24ce87c0452f9fac
coderLin <1543099786@qq.com>
2025-09-04 19:30:36 +0800

    feat: 使用AiService接入聊天记忆

---
+++ src/test/java/com/example/ChatMemoryTest.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.service.AiServices;
+import org.example.XiaozhiApp;
+import org.example.assistant.Assistant;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = XiaozhiApp.class)
+public class ChatMemoryTest {
+
+    @Autowired
+    private QwenChatModel qwenChatModel;
+
+    @Test
+    public void testChatMemory() {
+        //创建chatMemory
+        MessageWindowChatMemory chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+
+        //创建AIService
+        Assistant assistant = AiServices
+                .builder(Assistant.class)
+                .chatLanguageModel(qwenChatModel)
+                .chatMemory(chatMemory)
+                .build();
+        //调用service的接口
+        String answer1 = assistant.chat("我是李白");
+        System.out.println(answer1);
+        String answer2 = assistant.chat("我是谁");
+        System.out.println(answer2);
+    }
+
+}
