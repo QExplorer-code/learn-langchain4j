c1582512f22dfbd1e5a338610f0192ce0aff1ee6
coderLin <1543099786@qq.com>
2025-09-04 19:37:25 +0800

    feat: 直接在Assistant接口中集成聊天记忆

--- src/test/java/com/example/ChatMemoryTest.java
+++ src/test/java/com/example/ChatMemoryTest.java
@@ -5,6 +5,7 @@ import dev.langchain4j.memory.chat.MessageWindowChatMemory;
 import dev.langchain4j.service.AiServices;
 import org.example.XiaozhiApp;
 import org.example.assistant.Assistant;
+import org.example.assistant.MemoryChatAssistant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,4 +34,13 @@ public class ChatMemoryTest {
         System.out.println(answer2);
     }
 
+    @Autowired
+    private MemoryChatAssistant memoryChatAssistant;
+    @Test
+    public void testChatMemory2() {
+        String answer1 = memoryChatAssistant.chat("我是李大白");
+        System.out.println(answer1);
+        String answer2 = memoryChatAssistant.chat("我是谁");
+        System.out.println(answer2);
+    }
 }
