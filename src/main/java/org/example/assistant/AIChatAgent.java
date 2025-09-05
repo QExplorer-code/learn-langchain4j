2dbc34019ae253461a591307d4003c937f2dad38
coderLin <1543099786@qq.com>
2025-09-05 15:43:27 +0800

    feat: 项目配置流式输出

--- src/main/java/org/example/assistant/AIChatAgent.java
+++ src/main/java/org/example/assistant/AIChatAgent.java
@@ -4,12 +4,14 @@ import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
+import reactor.core.publisher.Flux;
 
 import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 
 @AiService(
         wiringMode = EXPLICIT,
-        chatModel = "qwenChatModel",
+//        chatModel = "qwenChatModel",
+        streamingChatModel = "qwenStreamingChatModel",
         chatMemoryProvider = "chatMemoryProviderChat",
         tools = "appointmentTools", // 配置tools
         contentRetriever = "contentRetrieverChat" // 配置向量存储
@@ -17,5 +19,5 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
 public interface AIChatAgent {
 
     @SystemMessage(fromResource = "AI-chat-prompt-template.txt")
-    String chat(@MemoryId Long memoryId, @UserMessage String userMessage);
+    Flux<String> chat(@MemoryId Long memoryId, @UserMessage String userMessage);
 }
--- src/main/java/org/example/controller/AIChatAgentController.java
+++ src/main/java/org/example/controller/AIChatAgentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @Tag(name = "AI助手")
 @RestController
@@ -18,8 +19,8 @@ public class AIChatAgentController {
     private AIChatAgent aiChatAgent;
 
     @Operation(summary = "对话")
-    @PostMapping("/chat")
-    public String chat(@RequestBody ChatForm chatForm)  {
+    @PostMapping(value = "/chat", produces = "text/stream;charset=utf-8") // 添加prodeces流式输出方式
+    public Flux<String> chat(@RequestBody ChatForm chatForm)  {
         return aiChatAgent.chat(chatForm.getMemoryId(), chatForm.getMessage());
     }
 }
